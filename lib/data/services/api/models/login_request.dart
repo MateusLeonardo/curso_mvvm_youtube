42d13382301225fff49719d707aa7977158f6397
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-06-13 18:50:04 -0300

    Aula 04: Serviços parte 1

---
+++ lib/data/services/api/models/login_request.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+class LoginRequest {
+  final String username;
+  final String password;
+  final String expiresInMins;
+
+  const LoginRequest({
+    required this.username,
+    required this.password,
+    required this.expiresInMins,
+  });
+
+  Map<String, dynamic> toJSon() {
+    return <String, dynamic>{
+      'username': username,
+      'password': password,
+      'expiresInMins': expiresInMins,
+    };
+  }
+}
